18d49cf923e08e521ab1955ed69bbb6e8cfec9e2
returnzero1-0 <returntozeroinit@gmail.com>
2021-05-25 23:27:47 +0530

    V-25052021

---
+++ All_Programming/DS/ds_using_c/array/HackerRank/2d_array.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int i,j,sum=0,max,flag=1;
+int arr[6][6];
+
+
+void hourglassSum(int arr[6][6]);
+
+int main()
+{
+    
+
+    for(i=0;i<6;i++)
+    {
+        for(j=0;j<6;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+
+    hourglassSum(arr);
+
+return 0;
+}
+
+void hourglassSum(int arr[6][6])
+{
+    for(i=1;i<5;i++)
+    {
+        for(j=1;j<5;j++)
+        {
+            sum=arr[i][j]+arr[i-1][j-1]+arr[i-1][j]+arr[i-1][j+1]+arr[i+1][j-1]+arr[i+1][j]+arr[i+1][j+1];
+
+            if(flag==1)
+            {
+                max=sum;
+                flag=0;
+            }
+            if(max<sum)
+            {
+                max=sum;
+            }
+        }
+    }
+    printf("%d",max);
+
+}
