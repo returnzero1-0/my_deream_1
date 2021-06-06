0a24804ae61cce1a68011a78f5c4bf65122538bd
returnzero1-0 <returntozeroinit@gmail.com>
2021-06-06 18:46:47 +0530

    V-06062021

--- All_Programming/DS/ds_using_c/array/HackerRank/between_two_sets.c
+++ All_Programming/DS/ds_using_c/array/HackerRank/between_two_sets.c
@@ -26,8 +26,66 @@ int main()
 return 0;
 }
 
+/*
+Example INPUT : 
+
+2 3
+[2 4]
+[16 32 96]
+*/
+
 void getTotalX(int a[],int b[])
 {
-    int i;
-    
+   int i,j=0,k, a_last=a[n-1],flag=0,temp[100],count=0;
+
+   while(a_last <=b[0])     // runs till a_last will become b[0]
+   {
+       for(i=0;i<n;i++) //runs n'th times
+       {
+           if(a_last % a[i]==0)
+           {
+               flag=1;
+           }
+           else
+           {
+               flag=0;
+               break;
+           }  
+       }
+       if(flag==1)
+        {
+            temp[j]=a_last;
+            j++;
+        }
+       
+     a_last++;
+   }
+/*
+for(i=0;i<j;i++)
+        {
+            printf("%d ",temp[i]);
+        }
+*/
+
+   for(i=0;i<j;i++)         //will run j'th times
+   {
+       for(k=0;k<m;k++)     //will run k'th times
+       {
+           if(b[k] % temp[i] == 0)
+           {
+               flag=1;
+           }
+           else
+           {
+               flag=0;
+               break;
+           }
+       }
+       if(flag==1)
+       {
+           count++;
+        }
+   }
+   printf("%d",count);
+         
 }
