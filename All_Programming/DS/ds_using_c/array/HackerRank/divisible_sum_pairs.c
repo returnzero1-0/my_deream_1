f959c0ef7794dd6774427aef4616c22ffaeeab32
returnzero1-0 <returntozeroinit@gmail.com>
2021-06-06 16:23:53 +0530

    V-06062021

--- All_Programming/DS/ds_using_c/array/HackerRank/divisible_sum_pairs.c
+++ All_Programming/DS/ds_using_c/array/HackerRank/divisible_sum_pairs.c
@@ -2,11 +2,38 @@
 
 #include<stdio.h>
 
-void divisibleSumPairs();
+void divisibleSumPairs(int n,int arr[],int k);
 
 int main()
 {
-    int ;
+    int n,k;
+    scanf("%d %d",&n,&k);
+
+    int arr[n];
+    
+    int i;
+    for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+
+    divisibleSumPairs(n,arr,k);
+    
 
 return 0;
+}
+
+void divisibleSumPairs(int n,int arr[],int k)
+{
+    int i,j,count=0;
+
+    for(i=0;i<n;i++)
+    {
+        for(j=(i+1);j<n;j++)
+        {
+            if((arr[i]+arr[j]) % k == 0)
+            {
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
 }
