f70d728508263b82d8eedeb64a589ee0d367c424
returnzero1-0 <returntozeroinit@gmail.com>
2021-06-17 22:20:24 +0530

    V-17062021

---
+++ All_Programming/Java/DS/RotateArray.java
@@ -0,0 +1,41 @@
+class RotateArray
+{
+  void leftRotate(int arr[],int d,int n)
+  {
+    for(int i=0;i<d;i++)
+    {
+      leftRotateByOne(arr,n);
+    }
+  }
+  void leftRotateByOne(int arr[],int n)
+  {
+    int i,temp;
+    temp=arr[0];
+
+    for(i=0;i<n-1;i++)
+    {
+      arr[i]=arr[i+1];
+    }
+    arr[n-1]=temp;
+  }
+
+  void printArray(int arr[],int n)
+  {
+    for(int i=0;i<n;i++)
+    {
+      System.out.print(arr[i]+" ");
+    }
+  }
+
+
+  public static void main(String args[])      // main method
+  {
+    RotateArray rotate_array=new RotateArray();
+
+    int arr[]={1,2,3,4,5,6,7};
+
+    rotate_array.leftRotate(arr,2,7);
+    rotate_array.printArray(arr,7);
+  }
+
+}
