bf77cd74bf4de5779942240e61f690c6a3545bcd
returnzero1-0 <returntozeroinit@gmail.com>
2021-07-12 19:58:37 +0530

    V-12072021

--- All_Programming/HackerRank/cut_the_sticks.cpp
+++ All_Programming/HackerRank/cut_the_sticks.cpp
@@ -1,9 +1,83 @@
 #include<iostream>
 using namespace std;
 
+void cutTheSticks(int sticks[]);
+
+int length_of_cut(int sticks[]);
+
+int lenght_of_array;
 int main()
 {
+    cin>>lenght_of_array;
+    int sticks[lenght_of_array];
 
+    for(int i=0;i<lenght_of_array;i++)
+    cin>>sticks[i];
+
+    cutTheSticks(sticks);
 
 return 0;
 }
+
+void cutTheSticks(int sticks[])
+{
+
+    for(int i=0;i<lenght_of_array;i++)
+    {
+        int cut_length=length_of_cut(sticks);
+        int cut_count=0;
+
+
+        for(int j=0;j<lenght_of_array;j++)
+        {
+                if(sticks[j]!=0)
+                {
+                    sticks[j]=sticks[j]-cut_length;
+                    cut_count++;    
+
+                } 
+                if(sticks[j]<0)
+                {
+                    sticks[j]=0;
+                }
+              
+
+        }
+        if(cut_count!=0)
+        {
+            cout<<cut_count<<"\n";
+        }
+        
+    }  
+   
+}
+
+
+int length_of_cut(int sticks[])
+{
+    int small;
+    for(int j=0;j<lenght_of_array;j++)
+    {
+        if(sticks[j]>0)
+        {
+            small=sticks[j];
+
+        }
+    }
+    
+    for(int j=0;j<lenght_of_array;j++)
+    {
+        if(sticks[j]>0)
+        {
+            if(sticks[j]<small)
+            {
+                small=sticks[j];
+            }
+        }
+        
+    }
+return small;
+    
+}
+
+
